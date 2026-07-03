dc8bcda5d636318bc3b49da9dbfe9fc5df407674
logesh <logesh15ak@gmail.com>
2026-07-03 21:37:43 +0530

    y

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,43 @@
-fn main() -> color_eyre::Result<()> {
+use color_eyre::Result;
+use ratatui::{
+    DefaultTerminal, Frame,
+    crossterm::event::{self, Event},
+    widgets::{Block, Paragraph, Widget},
+};
+
+#[derive(Debug, Default)]
+struct AppState {
+    items: Vec<TodoList>,
+}
+
+#[derive(Debug, Default)]
+struct TodoList {
+    is_done: bool,
+    description: String,
+}
+
+fn main() -> Result<()> {
+    let state = AppState::default();
     color_eyre::install()?;
+    let terminal = ratatui::init();
+    let result = run(terminal);
+    ratatui::restore();
+    result
+}
+fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                event::KeyCode::Esc => break,
+                _ => {}
+            }
+        }
+    }
     Ok(())
+}
+
+fn render(frame: &mut Frame, app_state: &mut AppState) {
+    Paragraph::new("hello from the applications ").render(frame.area(), frame.buffer_mut());
+    Block::bordered().render(frame.area(), frame.buffer_mut());
 }
